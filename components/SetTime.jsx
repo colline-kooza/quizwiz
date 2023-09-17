6ca4064b9455b848b0ba107868d0686396330fed
colline-kooza <126054104+colline-kooza@users.noreply.github.com>
2023-09-18 01:50:33 +0300

    timer

--- components/SetTime.jsx
+++ components/SetTime.jsx
@@ -33,7 +33,7 @@ export default function Testing() {
 
     const hideTimer = setTimeout(() => {
       setDisplayH2(false);
-    }, 3000);
+    }, 4000);
 
     return () => {
       clearTimeout(showTimer);
@@ -44,7 +44,7 @@ export default function Testing() {
   return (
     <div>
       {displayH2 && (
-        <div className="w-[100%] h-[100vh] bg-slate-950 z-50 flex flex-col items-center gap-[3rem] lg:gap-[8rem]">
+        <div className="w-[100%] h-[100vh] bg-slate-950 z-50 flex flex-col items-center gap-[1rem] lg:gap-[8rem]">
           <div className="flex justify-center items-center ">
             <img
               className="w-[70%] h-[80vh] object-contain lg:h-[60vh] lg:w-[60%]"
